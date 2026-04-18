31d2a58662f151c8bf491a406b5ab4a278fe57d6
Jherson Sanchez <jhersonsanchez905@gmail.com>
2026-04-18 06:57:40 -0500

    fix: agregar CSRF_TRUSTED_ORIGINS para produccion en Render

--- config/settings.py
+++ config/settings.py
@@ -104,3 +104,7 @@ import dj_database_url
 DATABASE_URL = config('DATABASE_URL', default=None)
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://invpro-unipamplona.onrender.com',
+]
